9bf4557a33a0f7d1dac9a2f0a933a9c560183acc
Sidra Jabeen <sidrajabeen5454@gmail.com>
2021-10-08 21:07:45 +0500

    Initial Commit

---
+++ LawyerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LawyerApp
+//
+//  Created by Sidra Jabeen on 08/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

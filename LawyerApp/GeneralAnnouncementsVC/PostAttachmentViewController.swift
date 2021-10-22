4a93c96a33387b444db2f4b94dada9ab50bdc5d5
Sidra Jabeen <sidrajabeen5454@gmail.com>
2021-10-23 00:27:28 +0500

    Add Post Attachment VC

---
+++ LawyerApp/GeneralAnnouncementsVC/PostAttachmentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PostAttachmentViewController.swift
+//  LawyerApp
+//
+//  Created by Sidra Jabeen on 22/10/2021.
+//
+
+import UIKit
+
+class PostAttachmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LawyerApp/ProfileVC/ProfileTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileTableViewCell.swift
+//  LawyerApp
+//
+//  Created by Sidra Jabeen on 22/10/2021.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
